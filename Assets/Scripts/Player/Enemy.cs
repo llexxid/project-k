da14f4ec300e55292ff55b74ff4eda1b1dd3ab96
hyungjin <91019153+hyungjin-yu@users.noreply.github.com>
2026-02-06 18:42:12 +0900

    테스트 적 만들기

--- Assets/Scripts/Player/Enemy.cs
+++ Assets/Scripts/Player/Enemy.cs
@@ -6,10 +6,16 @@ public class Enemy : MonoBehaviour
 {
     public Enemy enemy;
 
+    void Start()
+    {         
+        enemy = GetComponent<Enemy>();
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            Debug.Log("Player¿Í Ãæµ¹!");
             enemy.gameObject.SetActive(false);
         }
     }
