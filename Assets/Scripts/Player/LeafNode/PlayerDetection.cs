c90b21f9a20ffa4458145e22f29cfa62960a4c99
hyungjin <91019153+hyungjin-yu@users.noreply.github.com>
2026-02-06 12:55:05 +0900

    플레이어블 캐릭터 기초 틀 잡기

---
+++ Assets/Scripts/Player/LeafNode/PlayerDetection.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetection : MonoBehaviour
+{
+    public void Detect()
+    {
+
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Detect();
+    }
+}
