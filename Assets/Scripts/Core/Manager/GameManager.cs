0ac3085d66ac94e32c96716f505bbf9d4a901e32
llexxid <101461320+llexxid@users.noreply.github.com>
2026-03-12 18:26:30 +0900

    몬스터 체력 정상화

--- Assets/Scripts/Core/Manager/GameManager.cs
+++ Assets/Scripts/Core/Manager/GameManager.cs
@@ -1,4 +1,4 @@
-using Cysharp.Threading.Tasks;
+﻿using Cysharp.Threading.Tasks;
 using Cysharp.Threading.Tasks.CompilerServices;
 using ExcelDataReader;
 using Scripts.Core.SO;
@@ -321,7 +321,7 @@ namespace Scripts.Core
 					foreach (var spawnPos in group)
 					{
 						MonsterSpawner.Instance.SpawnMonster(eMonsterType.MON_ORC, spawnPos, Quaternion.identity, out Monster mon);
-						MonsterStat stat = new MonsterStat(100000, 0, (ulong)monInfo._baseAtk, monInfo._baseMoveSpeed, monInfo._baseAtkSpeed);
+						MonsterStat stat = new MonsterStat(monInfo._baseHp, 0, (ulong)monInfo._baseAtk, monInfo._baseMoveSpeed, monInfo._baseAtkSpeed);
 						mon.Init(eMonsterType.MON_ORC, stat, monInfo._dropTableNumber);
 					}
 				}
